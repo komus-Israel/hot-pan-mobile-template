1a9210cfc595017edf9a84c5cafb1c4b7b946e33
komus-Israel <komolehinIsrael@gmail.com>
2021-10-09 10:21:39 +0100

    refactored the food product page to food card such that the page is a map of food card

--- lib/components/customClasses.dart
+++ lib/components/customClasses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../routes/routes.dart';
 
 class TabRouteObject{
 
@@ -97,47 +98,6 @@ class _buildFormState extends State<buildForm> {
   }
 }
 
-
-/*Widget buildLoginForm({size, String hint : 'Category', String inputType, prefixIcon, obscure:false, bool suffixIcon:false, action}){
-  return Padding(
-    padding: EdgeInsets.symmetric(vertical: 10.0),
-    child: Container(
-      //height: size.height * 0.08,
-      width: size.width * 0.8,
-      decoration: BoxDecoration(
-        color: Colors.grey[500].withOpacity(0.5),
-        borderRadius: BorderRadius.circular(10),
-      ),
-
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: TextFormField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: hint,
-                prefixIcon: prefixIcon,
-                suffixIcon: suffixIcon ? IconButton(
-                    icon: Icon(Icons.remove_red_eye_outlined),
-                  onPressed: action,
-                ) : null
-                //hintStyle: kBodyText,
-              ),
-
-            style: TextStyle(
-              color: Colors.white
-            ),
-            cursorColor: Colors.amber,
-            obscureText: obscure,
-
-
-          ),
-        ),
-      ),
-    ),
-  );
-}*/
-
 Widget buildActionButton(action){
   return Container(
     height: 60,
@@ -180,4 +140,123 @@ Widget BackgroundImage(image){
       ),
     ),
   );
+}
+
+class FoodCard extends StatefulWidget {
+
+  Map food;
+  @override
+  FoodCard({Key key, @required this.food}) : super(key:key);
+  _FoodCardState createState() => _FoodCardState();
+}
+
+class _FoodCardState extends State<FoodCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+      child: Column(
+        children:  <Widget>[
+      Stack(
+
+      children: <Widget>[
+        Container(
+        height: 200,
+        width: 500,
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          child: Image(
+            fit: BoxFit.cover,
+            image: AssetImage(widget.food['image']),
+          ),
+        ),
+      ),
+
+      Positioned(
+        bottom: 0,
+        child: Container(
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20)
+
+            ),
+            color: Color.fromRGBO(248, 248, 248, 1),
+          ),
+
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'N' + widget.food['price'],
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+
+            ),
+          ),
+        ),
+      ),
+      ],
+    ),
+    Container(
+          margin: EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+
+                  children: <Widget>[
+                        Row(
+
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+
+                          Text(
+                            widget.food['name'],
+                         style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                       ),
+                      ),
+
+                     TextButton(
+                   onPressed: ()=>namedPageRoute(
+                              context,
+                              '/order',
+                              {'name': 'chicken stew', 'image': 'assets/images/img1.jpg', 'price': '4 ,000', 'averageRating': '3.5', 'address':'ilorin'}
+                          ),
+                           child: Text('Order'),
+                          style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                           backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                      ),
+                  )
+                  ]
+       ),
+                  Container(
+                         margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+
+                          child: Row(
+
+                            children: [
+                                         Icon(
+                                                 Icons.star_rate_rounded,
+                                                   size: 17,
+                                                    color: Color.fromRGBO(237, 161, 69, 1),
+                                         ),
+                          Text(
+                      widget.food['averageRating']
+                    ),
+
+                   ],
+              ),
+    )
+                    ]
+      )
+                    )
+    ]
+      )
+                    );
+  }
 }
